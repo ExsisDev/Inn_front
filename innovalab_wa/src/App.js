dea71861b91a3c9f928416320e8a8bb6caa0ba46
dagofonseca <dagofonseca6@gmail.com>
2019-11-28 15:10:35 -0500

    home modified

--- innovalab_wa/src/App.js
+++ innovalab_wa/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from "./components/landing/Landing";
 import Home from "./components/home/Home";
-
 import './App.css';
 
 function App() {
