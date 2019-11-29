f2130296bd8adfdc0a2093818385d886f91f56ab
dagofonseca <dagofonseca6@gmail.com>
2019-11-29 12:00:20 -0500

    header for back navigation ws extracted to a new component

---
+++ innovalab_wa/src/components/backNavigator/BackNavigator.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Row, Col, Button, Image } from 'react-bootstrap';
+import backButton from '../../images/backButton.png';
+import innovaCamaraLogo from '../../images/innovaCamaraLogo.png';
+
+const BackNavigator = () => {
+    let history = useHistory();
+    return ( 
+        <Row className="my-3">
+            <Col md={2}>
+                <Button variant="link" className="backButton" onClick={()=>history.goBack()}>
+                    <Image src={backButton} alt="Back button" 
+                           className="backButtonImg" />
+                    Volver                    
+                </Button>
+            </Col>
+            <Col md={{ span: 4, offset: 6 }}>
+                <div className="camaraLogoBox ml-auto">
+                    <img className="camaraLogo align-self-center" src={innovaCamaraLogo} alt="logo icon" />
+                </div>
+            </Col>
+        </Row> 
+    );
+}
+ 
+export default BackNavigator;
