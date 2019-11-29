ca1c5ffd3847f74070052e9e6ff798886e81811a
dagofonseca <dagofonseca6@gmail.com>
2019-11-29 17:58:57 -0500

    Create ally in progress. Some form groups added

--- innovalab_wa/src/components/ally/CreateAlly.js
+++ innovalab_wa/src/components/ally/CreateAlly.js
@@ -1,34 +1,115 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Form } from 'react-bootstrap';
+import BackNavigator from '../utilities/backNavigator/BackNavigator';
+import SectionTitle from '../utilities/sectionTitle/SectionTitle';
 import logoCrear from '../../images/RetosTerminados.png';
-
 import "./CreateAlly.css"
+
+const possibleCategories = [
+    "Aplicación Móvil",
+    "iOS",
+    "Realidad Aumentada",
+    "Blockchain",
+    "Transformación Digital",
+    "Desarrollo Móvil",
+    "UX/UI"
+]
 
 class CreateAlly extends React.Component {
     constructor() {
         super();
         this.state = {
+            categories: []
         }
     }
-
+    fillSelectedElement = ( event ) => {
+        let selectedElement = event.target.value;        
+        let currentCategories = this.state.categories;
+        
+        if(!currentCategories.includes(selectedElement)){
+            currentCategories.push(selectedElement);
+            this.setState({categories: currentCategories})
+        }
+        console.log(this.state.categories);
+    }
     render() {
-        return(
-            <Container>
-                <Row>
-                    <Col>
-                        aldo
-                    </Col>                    
-                </Row>
+        const titleProps = {
+            text: "Crear Nuevo Aliado",
+            img: logoCrear,
+            imgAlt: "logo crear nuevo aliado"
+        }
+        return (
+            <Container className="d-flex flex-column align-items-center">
+                <BackNavigator />
+                <SectionTitle titleProps={titleProps} />
+                <Row className="my-3">
+                    <Form>
+                        <Form.Row className="m-0">
+                            <Col>
+                                <span>Datos de la empresa: </span>
+                                <Form.Group controlId="companyName">
+                                    <Form.Control placeholder="Nombre de la Empresa" />
+                                </Form.Group>
+                                <Form.Group controlId="nit">
+                                    <Form.Control placeholder="NIT" />
+                                </Form.Group>
+                                <Form.Group controlId="companyEmail">
+                                    <Form.Control placeholder="Email" />
+                                </Form.Group>
+                                <Form.Group controlId="webSite">
+                                    <Form.Control placeholder="Página Web" />
+                                </Form.Group>
+                                <Form.Group controlId="companyPhone">
+                                    <Form.Control placeholder="Teléfono" />
+                                </Form.Group>
+                            </Col>
 
-                <Row>
-                    <Col>
-                        <img src={logoCrear} alt="logo crear aliado" />
-                        <h2>Crear Aliado</h2>
-                    </Col>
-                </Row>
+                            <Col>
+                                <Form.Group as={Row} className="mx-0" controlId="categories">
+                                    <Col>
+                                        <Form.Label>
+                                            Categorías de especialidad:
+                                        </Form.Label>
+                                        <p>{this.state.categories}</p>
+                                    </Col>
+                                    <Col>
+                                        <Form.Control as="select" onChange={this.fillSelectedElement}>
+                                            {possibleCategories.map( category => {
+                                                return <option key={category}>{category}</option>
+                                            })}
+                                        </Form.Control>
+                                    </Col>                                    
+                                </Form.Group>
 
-                <Row>
-                    <Col>c</Col>
+                                <Form.Group as={Row} className="mx-0" controlId="ideHours">
+                                    <Form.Label column>
+                                            Horas de ideación mensuales:
+                                    </Form.Label>
+                                    <Col>
+                                        <Form.Control type="number" />
+                                    </Col>
+                                </Form.Group>
+
+                                <Form.Group as={Row} className="mx-0" controlId="expHours">
+                                    <Form.Label column>
+                                            Horas de experimentación mensuales:
+                                    </Form.Label>
+                                    <Col>
+                                        <Form.Control type="number" />
+                                    </Col>
+                                </Form.Group>
+                            </Col>
+                        </Form.Row>
+                        <Form.Row className="m-0">
+                            <Col>
+                                c
+                            </Col>
+
+                            <Col>
+                                d
+                            </Col>
+                        </Form.Row>
+                    </Form>
                 </Row>
             </Container>
         );
--- innovalab_wa/src/components/utilities/sectionTitle/SectionTitle.js
+++ innovalab_wa/src/components/utilities/sectionTitle/SectionTitle.js
@@ -4,7 +4,7 @@ import './SectionTitle.css';
 
 const SectionTitle = (props) => {
     return ( 
-        <Row>
+        <Row className="my-3">
             <Col className="sectionTitleBox d-flex justify-content-center">
                 <img className="sectionTitleLogo" src={props.titleProps.img} alt={props.titleProps.imgAlt} />
                 <h3 className="sectionTitleText w-auto ml-2 align-self-center">{props.titleProps.text}</h3>
