4b720ddee0d642f0b21a265a022496fa4f7eafa1
dagofonseca <dagofonseca6@gmail.com>
2019-12-11 17:48:10 -0500

    edit ally header finished

--- innovalab_wa/src/components/ally/EditAlly.js
+++ innovalab_wa/src/components/ally/EditAlly.js
@@ -1,20 +1,46 @@
 import React from 'react';
+import { Container, Row, Col, Button, Image } from 'react-bootstrap';
+import { IconContext } from "react-icons";
+import { IoIosLogOut } from 'react-icons/io';
 import BackNavigator from '../utilities/backNavigator/BackNavigator';
+import img from '../../images/EmpresaA.png';
 import './EditAlly.css';
 
 class EditAlly extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( 
-            <div>
-                <h1>Edit Ally</h1>
-                <BackNavigator />
-            </div>
+    render() {
+        return (
+            <Container className="p-0" fluid>
+                <Row className="m-0 bckgndEditAlly">
+                    <Col className="d-flex flex-column align-items-center pl-5">
+                        <BackNavigator dark />
+                    </Col>
+                    <Col md={{span: 4, offset: 4}} className="companyImageCol mt-5">
+                        <Image className="imgEditAlly" src={img}
+                            alt="logo de la compañia"
+                            roundedCircle
+                        />
+                        <h3 className="companyNameEditAlly mt-4">Empresa A</h3>
+                    </Col>
+                </Row>
+                <Row className="m-0 bckgndEditAlly">
+                    <Col sm="2" className="pl-4 logoutBox" >
+                        <Button variant="link"
+                            className="logoutBtn d-flex align-items-center"
+                        >
+                            <IconContext.Provider value={{ color: "white", size: "2rem", className: "logoutIcon" }}>
+                                <IoIosLogOut />
+                            </IconContext.Provider>
+                            Cerrar Sesión
+                        </Button>
+                    </Col>                    
+                </Row>
+            </Container>
         );
     }
 }
- 
+
 export default EditAlly;
--- innovalab_wa/src/components/utilities/backNavigator/BackNavigator.js
+++ innovalab_wa/src/components/utilities/backNavigator/BackNavigator.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { Row, Col, Button, Image } from 'react-bootstrap';
-import backButton from '../../../images/backButton.png';
+import backButtonImg from '../../../images/backButton.png';
+import backButtonBlackImg from '../../../images/backButtonBlack.PNG';
 import innovaCamaraLogo from '../../../images/innovaCamaraLogo.png';
+import innovaCamaraLogoBlack from '../../../images/innovaCamaraLogoBlack.PNG';
 import './BackNavigator.css';
 
-const BackNavigator = () => {
+const BackNavigator = (props) => {
     let history = useHistory();
+    let black = "";
+    let logo = innovaCamaraLogo;
+    let backBtn = backButtonImg;
+
+    if(props.dark){
+        black = "black";
+        logo = innovaCamaraLogoBlack;
+        backBtn = backButtonBlackImg;
+    }        
+    
     return ( 
-        <Row className="my-3 align-items-center">
+        <Row className={`my-3 align-items-center ${black}`}>
             <Col md={2} className="p-0">
-                <Button variant="link" className="backButton px-0 d-flex justify-content-start align-items-center" onClick={()=>history.goBack()}>
-                    <Image src={backButton} alt="Back button" 
+                <Button variant="link" 
+                        className={`backButton px-0 d-flex justify-content-start align-items-center ${black}`} 
+                        onClick={()=>history.goBack()}
+                >
+                    <Image src={backBtn} alt="Back button" 
                            className="backButtonImg" />
                     Volver
                 </Button>
             </Col>
-            <Col md={{ span: 4, offset: 6 }}>
+            <Col md={{ span: 3, offset: 7 }}>
                 <div className="ml-auto">
-                    <img className="align-self-center" src={innovaCamaraLogo} alt="logo icon" />
+                    <img className="align-self-center logo" src={logo} alt="logo icon" />
                 </div>
             </Col>
         </Row> 
