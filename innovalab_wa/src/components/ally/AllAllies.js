3f463188d0fd2b0a92964cddbfa11460685c4c72
dagofonseca <dagofonseca6@gmail.com>
2019-12-19 07:40:56 -0500

    AllAllies get allies from back

--- innovalab_wa/src/components/ally/AllAllies.js
+++ innovalab_wa/src/components/ally/AllAllies.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { Container, Row, Col, Table } from 'react-bootstrap';
 import BackNavigator from '../utilities/backNavigator/BackNavigator';
 import SectionTitle from '../utilities/sectionTitle/SectionTitle';
@@ -6,6 +7,61 @@ import logoExc from '../../images/exclamación.png';
 import './AllAllies.css';
 
 class AllAlies extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            allies: [],
+            token: this.getToken()
+        }
+    }
+
+    componentDidMount() {
+        if (this.state.token) {
+            this.getAlliesByPage(1);
+        }
+    }
+
+    /**
+     * Obtener el token desde localStorage
+     * @return {String} token 
+     */
+    getToken() {
+        return localStorage.getItem('auth-token');
+    }
+
+    /**
+    * Obtener todos los aliados por página
+    * @return {Object} categories
+    */
+    getAlliesByPage(page) {
+        const url = `${process.env.REACT_APP_BACK_URL}/allies/page/${page}`;
+
+        axios.get(url, {
+            headers: { 'x-auth-token': `${this.state.token}` }
+        })
+            .then(res => {
+                this.setState({ allies: res.data });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    renderAllies = (allies) => {
+        return (
+            allies.map(ally => {
+                return (
+                    <tr key={ally.id_ally}>
+                        <td>{ally.ally_name}</td>
+                        <td>{ally.ally_month_ideation_hours}</td>
+                        <td>{ally.ally_month_experimentation_hours}</td>
+                        <td>2</td>
+                        <td>2</td>
+                    </tr>
+                );
+            })           
+        );
+    }
 
     render() {
         const titleProps = {
@@ -13,11 +69,12 @@ class AllAlies extends React.Component {
             img: logoExc,
             imgAlt: "logo administrar aliados"
         }
+        let allies = this.state.allies;
         return (
             <Container className="d-flex flex-column align-items-center px-5">
 
-                    <BackNavigator />
-                    <SectionTitle titleProps={titleProps} />
+                <BackNavigator />
+                <SectionTitle titleProps={titleProps} />
                 <Row className="my-3 formBox paddingBox"    >
                     <Table borderless hover>
                         <thead>
@@ -30,20 +87,7 @@ class AllAlies extends React.Component {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                                <td>@fat</td>
-                            </tr>                            
+                            {this.renderAllies(allies)}
                         </tbody>
                     </Table>
                 </Row>
