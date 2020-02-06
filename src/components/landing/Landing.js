4a7de1b5f1e1fbbad2f2ccabe80fee73102473e0
Test <lalosi3753@tmailcloud.com>
2020-02-06 14:53:26 -0500

    Fixed until now Landing, LoginForm, RecoverNewPassword, RecoverPasswordEmail

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -23,26 +23,6 @@ const Landing = () => {
     } else {
 
         return (
-            // <Container className="landing d-flex flex-column align-items-center" fluid>
-            //     <Row id="landingFirstRow" className="d-flex justify-content-center align-items-center">
-            //         <Col md={5} xl={4} lg={4} sm={6} className="landingMain">
-            //             <div className="landingBorder">
-            //                 <Switch>
-            //                     <Route path="/" exact component={WelcomeText} />
-            //                     <Route path="/login" exact component={LoginForm} />
-            //                     <Route path="/recover-password/email" exact component={RecoverPasswordEmail} />
-            //                     <Route path="/recover-password" exact component={RecoverNewPassword} />
-            //                     <Redirect from="*" to="/pageNotFound" />
-            //                 </Switch>
-            //             </div>
-            //         </Col>
-            //     </Row>
-            //     <Row className="justify-content-center">
-            //         <Col xs={6} md={4} lg={4} className="landingImgLogo">
-            //             <Image src={innovaCamarLogo} alt="logo innovalab" fluid />
-            //         </Col >
-            //     </Row>
-            // </Container>
             <Container fluid className="landing">
                 <Row className="landingLoginBox d-flex justify-content-center ">
                     <Col lg={{ span: 4 }} md={{ span: 6 }} sm={{ span: 8 }} xs={{ span: 10 }} className=" d-flex align-items-center">
@@ -53,6 +33,7 @@ const Landing = () => {
                                     <Route path="/login" exact component={LoginForm} />
                                     <Route path="/recover-password/email" exact component={RecoverPasswordEmail} />
                                     <Route path="/recover-password" exact component={RecoverNewPassword} />
+                                    <Redirect from="*" to="/pageNotFound" />
                                 </Switch>
                             </div>
                         </div>
@@ -61,7 +42,7 @@ const Landing = () => {
                 <Row className="landingImage d-flex justify-content-center">
                     <Col lg={{ span: 5 }} md={{ span: 7 }} sm={{ span: 9 }} xs={{ span: 11 }}>
                         <div className="landingImageBox">
-                            <Image src={innovaCamarLogo} alt="logo innovalab" height="100%" width="100%"/>
+                            <Image src={innovaCamarLogo} alt="logo innovalab" height="100%" width="100%" />
                         </div>
                     </Col>
                 </Row>
